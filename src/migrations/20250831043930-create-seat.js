7c14eb4106f6071e090475ccab537973282b1946
Tushar Agarwal <tushar7436@gmail.com>
2025-08-31 11:23:22 +0530

    Added Seats model to db associated to airplane

---
+++ src/migrations/20250831043930-create-seat.js
@@ -0,0 +1,52 @@
+'use strict';
+const { BelongsTo } = require('sequelize');
+const { ENUMS } = require('../utils/common');
+const { BUSINESS, ECONOMY, PREMIUM_ECONOMY,FIRST_CLASS } = ENUMS.SEAT_TYPE;
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Seats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      airplaneId: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references: {
+          model: 'Airplanes',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+      },
+      row: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      col: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      type: {
+        type:Sequelize.ENUM,
+        values:[BUSINESS, ECONOMY, PREMIUM_ECONOMY, FIRST_CLASS],
+        default:ECONOMY,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Seats');
+  }
+};
---
+++ src/models/seat.js
@@ -0,0 +1,59 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+const { ENUMS } = require('../utils/common');
+const { BUSINESS, ECONOMY, PREMIUM_EONOMY,FIRST_CLASS } = ENUMS.SEAT_TYPE;
+module.exports = (sequelize, DataTypes) => {
+  class Seat extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+     this.belongsTo(models.Airplane, {
+        foreignKey: 'airplaneId',
+      });
+
+    }
+  }
+  Seat.init({
+    airplaneId:{
+      type:DataTypes.INTEGER,
+      alloNull: false
+    },
+    row:{
+      type: DataTypes.INTEGER,
+      allowNull:false
+    },
+    col:{
+      type: DataTypes.INTEGER,
+      allowNull:false
+    },
+    type:{
+      type:DataTypes.ENUM,
+      values:[BUSINESS, ECONOMY, PREMIUM_EONOMY, FIRST_CLASS],
+      default:ECONOMY
+    }
+  }, {
+    sequelize,
+    modelName: 'Seat',
+  });
+  return Seat;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
