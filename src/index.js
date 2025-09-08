e9b1d143e7d46b623642c804515e8058fe13c7e3
Tushar Agarwal <tushar7436@gmail.com>
2025-09-09 02:38:58 +0530

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const cors = require('cors');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
+
+
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());   
 app.use(express.urlencoded({extended: true}));
