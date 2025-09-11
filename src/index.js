257133d3f8ebce369e298634e9f163ee7226b340
Tushar Agarwal <tushar7436@gmail.com>
2025-09-11 09:39:23 +0530

    changed CORS Policy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const apiRoutes = require('./routes');
 const app = express();
 
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 
 app.use(express.json());   
 app.use(express.urlencoded({extended: true}));
